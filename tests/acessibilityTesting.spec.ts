d4624541ed368c7febf3ad466b60bded9203d129
rvkushch <rv.kushch@gmail.com>
2025-02-19 02:11:45 +0200

    added dotenv dependency

--- tests/acessibilityTesting.spec.ts
+++ tests/acessibilityTesting.spec.ts
@@ -1,10 +1,9 @@
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-test.skip('homepage', () => { 
-  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    await page.goto('https://your-site.com/');
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); 
-    expect(accessibilityScanResults.violations).toEqual([]);
-  });
+// there are a lot of issues on google homepage so this test is marked with 'fail' annotation
+test.fail('should not have any automatically detectable accessibility issues', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
